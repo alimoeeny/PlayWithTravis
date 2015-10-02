7641483f5c81c78dd2c3dfb3a85813056e3ab340
Ali Moeeny <alimoeeny@gmail.com>
2015-10-02 09:53:35 -0400

    getting started

---
+++ PlayWithTravis/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PlayWithTravis
+//
+//  Created by Ali Moeeny on 10/2/15.
+//  Copyright © 2015 Ali Moeeny. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
