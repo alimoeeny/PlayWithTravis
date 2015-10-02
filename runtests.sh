aaa2a8cb84c9244db2310cefde0b91ce78d1d7cf
Ali Moeeny <alimoeeny@gmail.com>
2015-10-02 14:27:54 -0400

    let's not pretty print it to get correct exit values

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,6 @@ xcodebuild  -workspace PlayWithTravis.xcworkspace \
             -scheme PlayWithTravis \
             -sdk iphonesimulator9.0 \
             -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.0' \
-            test | xcpretty --test --color
+            test 
+
+#| xcpretty --test --color
