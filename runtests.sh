d3e1fa680fb58999d1ba67dbcc95d6342e1e6977
Ali Moeeny <alimoeeny@gmail.com>
2015-10-02 14:22:46 -0400

    correct version for travis

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xcodebuild  -workspace PlayWithTravis.xcworkspace \
             -scheme PlayWithTravis \
-            -sdk iphonesimulator9.1 \
-            -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.1' \
+            -sdk iphonesimulator9.0 \
+            -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.0' \
             test | xcpretty --test --color
